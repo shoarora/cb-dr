03ca844e6d48082c726d77ed80e8bae5af51b333
Jeff Hara <jhara_mac@DN51ro6e.SUNet>
2017-11-11 12:55:51 -0800

    glove vectors

--- download_datasets.sh
+++ download_datasets.sh
@@ -6,21 +6,21 @@ cd data
 wget http://www.uni-weimar.de/medien/webis/corpora/corpus-webis-clickbait-17/clickbait17-train-170331.zip
 wget http://www.uni-weimar.de/medien/webis/corpora/corpus-webis-clickbait-17/clickbait17-train-170630.zip
 
-wget https://nlp.stanford.edu/software/GloVe-1.2.zip
+wget https://nlp.stanford.edu/data/GloVe.6B.zip
 
 unzip clickbait17-train-170331.zip
 unzip clickbait17-train-170630.zip
 
-unzip GloVe-1.2.zip
+unzip GloVe.6B.zip
 
 rm clickbait17-train-170331.zip
 rm clickbait17-train-170630.zip
 
-rm GloVe-1.2.zip
+rm GloVe.6B.zip
 
 mv clickbait17-train-170331 cb-small
 mv clickbait17-validation-170630 cb-big
 
-mv GloVe-1.2 glove
+mv GloVe.6B glove
 
 cd ../
