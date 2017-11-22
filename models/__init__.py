878d2450de4581e30df7cf6a1974cba81a8bb9f9
shoarora <shoarora@stanford.edu>
2017-11-22 00:02:50 -0800

    import nn classes

--- models/__init__.py
+++ models/__init__.py
@@ -3,12 +3,18 @@ from logisticRegression import LogisticRegression
 from bayesianRidgeRegression import BayesianRidge
 from kernelRidgeRegression import KernelRidgeRegression
 from svm import SVM
+from nn import VanillaNN
+from rnn import RNN
+from cnn import CNN
 import feature_extraction as FE
 
 
 model_options = {
     'no': AlwaysNo,
-    'lr': LogisticRegression
+    'lr': LogisticRegression,
+    'nn': VanillaNN,
+    'rnn': RNN,
+    'cnn': CNN
 }
 
 sk_model_options = {
