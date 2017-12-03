fe56d00df1e32ba230994bc97d0939ad76afba78
shoarora <shoarora@tibet4.stanford.edu>
2017-12-03 03:08:50 -0800

    bug fixes

--- models/cnn.py
+++ models/cnn.py
@@ -19,18 +19,17 @@ class CNN(TorchBase):
         self.load_glove()
         self.convs = nn.ModuleList([nn.Conv2d(1, KERNEL_DIM, (K, INPUT_DIM))
                                     for K in KERNEL_SIZES])
-        self.linear = nn.Linear(None, 1)  # TODO what dim?
+        self.linear = nn.Linear(300, 1)  # TODO what dim?
 
     def preprocess_inputs(self, inputs, ids, path):
         new_inputs = get_word_ids(inputs, self.vocab, self.num_words)
         return new_inputs
 
     def forward(self, x):
-        x = self.embedding(x).unsqueeze(1)  # (B,1,T,D)
+        x = self.embedding(x.long()).unsqueeze(1)  # (B,1,T,D)
         x = [F.relu(conv(x)).squeeze(3) for conv in self.convs]
         x = [F.max_pool1d(i, i.size(2)).squeeze(2) for i in x]
 
         concated = torch.cat(x, 1)
         concated = self.dropout(concated)
-
-        return self.linear(x)
+        return self.linear(concated)
