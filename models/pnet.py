f0110a678efdf052d07926681e249e8b5d941f8e
shoarora <shoarora@stanford.edu>
2017-12-10 02:10:44 -0800

    make pnet originate glove vectors

--- models/pnet.py
+++ models/pnet.py
@@ -50,6 +50,7 @@ class ParallelNet(TorchBase):
 
         x = self.embedding(x.long())
         y = self.embedding(y.long())
+        print x.size(), y.size()
 
         x = self.net1.extract_features(x)
         y = self.net2.extract_features(x)
