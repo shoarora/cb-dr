d9a9b0b53a06f2a4533a79dcc7ec874faff9dcea
shoarora <shoarora@stanford.edu>
2017-12-09 23:07:40 -0800

    take input arguments

--- models/cnn.py
+++ models/cnn.py
@@ -11,25 +11,35 @@ KERNEL_DIM = 100
 
 class CNN(TorchBase):
 
-    def __init__(self):
+    def __init__(self, input_dim=INPUT_DIM,
+                 kernel_sizes=KERNEL_SIZES, kernel_dim=KERNEL_DIM):
         super(CNN, self).__init__()
         self.num_words = 100
         self.dropout_p = 0.2
         self.dropout = nn.Dropout(self.dropout_p)
         self.load_glove()
-        self.convs = nn.ModuleList([nn.Conv2d(1, KERNEL_DIM, (K, INPUT_DIM))
-                                    for K in KERNEL_SIZES])
+
+        self.input_dim = input_dim
+        self.kernel_sizes = kernel_sizes
+        self.kernel_dim = kernel_dim
+
+        self.convs = nn.ModuleList([nn.Conv2d(1, kernel_dim, (K, input_dim))
+                                    for K in kernel_sizes])
         self.linear = nn.Linear(300, 1)  # TODO what dim?
 
     def preprocess_inputs(self, inputs, ids, path):
         new_inputs = get_word_ids(inputs, self.vocab, self.num_words)
         return new_inputs
 
-    def forward(self, x):
+    def extract_features(self, x):
         x = self.embedding(x.long()).unsqueeze(1)  # (B,1,T,D)
         x = [F.relu(conv(x)).squeeze(3) for conv in self.convs]
         x = [F.max_pool1d(i, i.size(2)).squeeze(2) for i in x]
 
         concated = torch.cat(x, 1)
         concated = self.dropout(concated)
-        return self.linear(concated)
+        return concated
+
+    def forward(self, x):
+        x = self.extract_features(x)
+        return self.linear(x)
--- models/rnn.py
+++ models/rnn.py
@@ -7,7 +7,7 @@ HIDDEN_SIZE = 256
 
 
 class RNN(TorchBase):
-    def __init__(self):
+    def __init__(self, input_dim=INPUT_DIM, hidden_size=HIDDEN_SIZE):
         super(RNN, self).__init__()
         self.load_glove()
 
@@ -15,21 +15,29 @@ class RNN(TorchBase):
         self.input_dropout_p = 0.2
         self.n_layers = 4
         self.bidirectional = False
-        self.rnn = nn.GRU(INPUT_DIM,
-                          HIDDEN_SIZE,
+
+        self.input_dim = input_dim
+        self.hidden_size = hidden_size
+
+        self.rnn = nn.GRU(input_dim,
+                          hidden_size,
                           self.n_layers,
                           batch_first=True,
                           dropout=self.input_dropout_p,
                           bidirectional=self.bidirectional)
-        self.linear = nn.Linear(self.n_layers * HIDDEN_SIZE, 1)
+        self.linear = nn.Linear(self.n_layers * hidden_size, 1)
 
     def preprocess_inputs(self, inputs, ids, path):
         new_inputs = get_word_ids(inputs, self.vocab, self.max_len)
         return new_inputs
 
-    def forward(self, x):
+    def extract_features(self, x):
         x = self.embedding(x.long())
         _, h = self.rnn(x)
         h = h.permute(1, 0, 2)
-        h = h.contiguous().view(-1, self.n_layers * HIDDEN_SIZE)
+        h = h.contiguous().view(-1, self.n_layers * self.hidden_size)
+        return h
+
+    def forward(self, x):
+        h = self.extract_features(x)
         return self.linear(h)
