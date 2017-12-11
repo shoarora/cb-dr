f3ed4fcc759b022069c5b1aad52105ac055a03a7
shoarora <shoarora@stanford.edu>
2017-12-11 00:05:58 -0800

    experiment scripts

---
+++ exp12-11/gpu0.sh
@@ -0,0 +1,1 @@
+python main.py --train --dataset big --cuda --gpu_num 0 --model rnn --sess_name big-rnn
---
+++ exp12-11/gpu1.sh
@@ -0,0 +1,1 @@
+python main.py --train --dataset big --cuda --gpu_num 1 --model cnn --sess_name big-cnn
