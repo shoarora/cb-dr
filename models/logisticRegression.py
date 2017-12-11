7f151bac1981a39ba384456088584bdf6d5df57f
Jeff Hara <jhara_mac@DN51ro29.SUNet>
2017-12-10 22:39:58 -0800

    skLearn LR

--- models/logisticRegression.py
+++ models/logisticRegression.py
@@ -1,16 +1,8 @@
-from pytorchbase import TorchBase
-from torch import nn
+from sklearn.linear_model import LogisticRegression as LR
+from skbase import SKBase
 
-INPUT_DIM = 9720
-OUTPUT_DIM = 1
-
-
-class LogisticRegression(TorchBase):
-    def __init__(self, input_dimension=INPUT_DIM, output_dimension=OUTPUT_DIM):
+class LogisticRegression(SKBase):
+    def __init__(self, choice=None, freq_floor=None):
         super(LogisticRegression, self).__init__()
-        self.logistic = nn.Linear(input_dimension, output_dimension)
-        self.sigmoid = nn.Sigmoid()
-
-    def forward(self, inputs):
-        out = self.logistic(inputs)
-        return self.sigmoid(out)
+        # support vector regression
+        self.model = LR()
