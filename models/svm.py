edc112b608346b6c6cb8324d6a6421b374a11241
Jeff Hara <jhara_mac@DN51ro29.SUNet>
2017-12-10 16:15:55 -0800

    added optional parameters.

--- models/svm.py
+++ models/svm.py
@@ -3,8 +3,8 @@ from skbase import SKBase
 
 
 class SVM(SKBase):
-    def __init__(self):
-        super(SVM, self).__init__()
+    def __init__(self, choice=None, freq_floor=None):
+        super(SVM, self).__init__(choice, freq_floor)
         # support vector regression
         self.model = svm.SVR()
 
