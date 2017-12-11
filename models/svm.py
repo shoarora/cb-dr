c2c0bbcf08856ae02f62e16750317d21e0b5f337
Jeff Hara <jhara_mac@DN51ro29.SUNet>
2017-12-10 22:30:14 -0800

    trying svr with 'linear' kernel (not Gaussian)

--- models/svm.py
+++ models/svm.py
@@ -6,7 +6,7 @@ class SVM(SKBase):
     def __init__(self, choice=None, freq_floor=None):
         super(SVM, self).__init__(choice, freq_floor)
         # support vector regression
-        self.model = svm.SVR()
+        self.model = svm.SVR(kernel='linear')
 
         self.num_epochs = 1
         self.batch_size = 25
