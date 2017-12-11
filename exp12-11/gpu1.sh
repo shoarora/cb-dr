f5232b02708baf07d938c29426273406378e920d
shoarora <shoarora@stanford.edu>
2017-12-11 00:53:44 -0800

    moved experiments

--- exp12-11/gpu1.sh
+++
@@ -1,1 +0,0 @@
-python main.py --train --dataset big --cuda --gpu_num 1 --model cnn --sess_name big-cnn
--- exp12-11/gpu2.sh
+++
@@ -1,1 +0,0 @@
-python main.py --train --dataset big --cuda --gpu_num 2 --model pnet3 --sess_name big-pnet3
