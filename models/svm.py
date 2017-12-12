acbc4e5e43a0c4e4be8bd1cc3a4a5c575c8b811b
Jeff Hara <jhara_mac@DN0a257161.SUNet>
2017-12-11 16:42:48 -0800

    normalize inputs

--- models/svm.py
+++ models/svm.py
@@ -1,6 +1,8 @@
 from sklearn import svm
 from skbase import SKBase
-
+from sklearn.preprocessing import MinMaxScaler
+X_train = scaling.transform(X_train)
+X_test = scaling.transform(X_test)
 
 class SVM(SKBase):
     def __init__(self, choice=None, freq_floor=None):
@@ -10,3 +12,13 @@ class SVM(SKBase):
 
         self.num_epochs = 1
         self.batch_size = 25
+
+    def fit(self, X, y):
+        scaling = MinMaxScaler(feature_range=(-1,1)).fit(X)
+        X = scaling.transform(X)
+        self.model.fit(X, y)
+
+    def predict(self, X, y):
+        scaling = MinMaxScaler(feature_range=(-1,1)).fit(X)
+        X = scaling.transform(X)
+        return self.model.predict(X)
